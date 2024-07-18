f206168629e5f638ba8e0a9852d6370db7d91dc3
Stroganov Ivan <gangun.24816@yandex.ru>
2024-07-18 22:47:07 +0300

    Create Lesson3

---
+++ Lesson3/Task3/Program.cs
@@ -0,0 +1,20 @@
+﻿void ZeroEventElements(int[] arr)
+{
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        if (arr[i] % 2 == 0)
+            arr[i] = 0;
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    foreach (int i in arr)
+    {
+        Console.Write($"{i} ");
+    }
+}
+
+int[] array = {1,2,3,4,5};
+ZeroEventElements(array);
+PrintArray(array); 
